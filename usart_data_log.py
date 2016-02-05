afb6420a7583efdb7c9b42c0ca4a07c4394d46aa
Reegan McAllister <reegan@ualberta.ca>
2016-02-04 17:08:00 -0700

    update

--- usart_data_log.py
+++ usart_data_log.py
@@ -6,7 +6,28 @@ import datetime
 
 now = datetime.datetime.now()
 
-file = open(now.isoformat(),'w')
-file.write("New log file" + now.isoformat() + "\n\r")
+#try: 
+file = open('log' + str(now.year) + '_' + str(now.month) + '_' + str(now.day) + '_' + str(now.hour) + '_' + str(now.minute) + '_' + '.csv','w')
+file.write("New log file" + str(now.isoformat()) + "\n\r")
 
+ser = serial.Serial('COM5',115200,timeout=1)
+
+not_accepted_chars = ['b', 'n', '\\', 'r', "'"]
+
+
+   
+while 1:
+    line2 = ''
+    line = str(ser.readline())
+    for x in range(0,len(line)):
+        if line[x] in not_accepted_chars:
+            line2 = line2
+        else:
+            line2 = line2 + str(line[x])
+    file.write(str(line2))
+    file.write("\n\r")
+    print(line2)
+
+#except:
 file.close()
+ser.close()
