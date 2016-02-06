e8cc4215e42be107778b768769f9c034bfa07c56
Reegan McAllister <reegan@ualberta.ca>
2016-02-06 11:28:21 -0700

    U

--- usart_data_log.py
+++ usart_data_log.py
@@ -12,14 +12,21 @@ now = datetime.datetime.now()
 
 try: 
     file = open('log' + str(now.year) + '_' + str(now.month) + '_' + str(now.day) + '_' + str(now.hour) + '_' + str(now.minute) + '_' + '.csv','w')
-    file.write("New log file" + str(now.isoformat()) + "\n\r")
+    file.write("New Log File" + str(now.isoformat()) + "\n\r")
     
-    ser = serial.Serial('COM5',115200,timeout=1)
+    ser = serial.Serial('COM6',115200,timeout=1)
     
     accepted_chars = ['1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',',','-',' ','_']
 
     print("CTRL+c to quit")
 
+    start_freeze = 1
+
+    while start_freeze == 1:
+        line = ser.read(1);
+        if line == b'$':
+            start_freeze = 0
+    
     while 1:
         line2 = ''
         line = str(ser.readline())
@@ -27,7 +34,7 @@ try:
             if line[x] in accepted_chars:
                 line2 = line2 + line[x]
         file.write(str(line2))
-        file.write("\n\r")
+        file.write("\n")
         #print(line2)
 
 except:
